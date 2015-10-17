6ea438d60a4ee95b905dca355313befaa1a40721
VRGuy <yboger@sensics.com>
2015-10-17 18:01:59 -0400

    (split)Add file to indicate when BNO has data ready

---
+++ Source code/Embedded/src/DeviceDrivers/bno-hostif/src/bno_callbacks.h
@@ -0,0 +1,14 @@
+/*
+ * bno_callbacks.h
+ *
+ * Created: 10/17/2015 5:50:02 PM
+ *  Author: YSB
+ */ 
+
+
+#ifndef BNO_CALLBACKS_H_
+#define BNO_CALLBACKS_H_
+
+extern int bno_data_ready; // from bno_callbacks. Testing for optimization
+
+#endif /* BNO_CALLBACKS_H_ */

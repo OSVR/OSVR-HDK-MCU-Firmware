92cb99684ec89d28e666a85343fa3ffc4ecdd56f
Ryan Pavlik <ryan@sensics.com>
2016-07-20 17:35:37 -0500

    (split)Accounts for the fact that libhdk20 IsVideoExistingPolling calls HDMI_IsVideoExisting, we modify that to snatch updated state instead of asking ourselves.
    Also print debug data when the empty function libhdk20 wants is called.

--- Source code/Embedded/src/DeviceDrivers/VideoInput_Toshiba_TC358870.c
+++ Source code/Embedded/src/DeviceDrivers/VideoInput_Toshiba_TC358870.c
@@ -16,6 +16,54 @@
 #include "Console.h"
 #include "SvrYield.h"
 
+#define SVR_DEBUG_LIBHDK2_BEHAVIOR
+
+/// @todo Despite this function being empty in the Coretronic fork (and the original contents since being renamed to be
+/// namespaced in the mainline), we need to define it libhdk20.a can link against it circularly...
+/// (IsVideoExistingPolling contains a reference to it)
+void UpdateResolutionDetection(void);
+void UpdateResolutionDetection()
+{
+#ifdef SVR_DEBUG_LIBHDK2_BEHAVIOR
+	if (VideoInput_Get_Status())
+	{
+		WriteLn("libhdk20: called UpdateResolutionDetection() when video signal present.");
+	}
+	else
+	{
+		WriteLn("libhdk20: called UpdateResolutionDetection() when video signal absent.");
+	}
+#endif
+}
+
+/// @todo We actually need to export this function as bool HDMI_IsVideoExisting(void) so libhdk20.a can link against it
+/// circularly... (IsVideoExistingPolling contains a reference to it)
+///
+/// Yes, this is effectively a duplicate of code in Toshiba_TC358870, except that we've interposed our signal report
+/// methods in it.
+///
+/// IsVideoExistingPolling calls this method.
+bool HDMI_IsVideoExisting(void);
+bool HDMI_IsVideoExisting()
+{
+	uint8_t tc_data;
+
+	if (TC358870_i2c_Read(0x8520, &tc_data) != TC358870_OK)
+	{  // get SYS_STATUS
+		VideoInput_Protected_Report_No_Signal();
+		return false;
+	}
+
+	if (tc_data != 0x9F)
+	{
+		VideoInput_Protected_Report_No_Signal();
+		return false;
+	}
+
+	VideoInput_Protected_Report_Signal();
+	return true;
+}
+
 void VideoInput_Init(void)
 {
 	// No separate init required - all done in Display_System_Init() since it's just one chip.
@@ -30,9 +78,9 @@ void VideoInput_Update_Resolution_Detection(void)
 
 void VideoInput_Task(void)
 {
+	// This method reportedly calls HDMI_IsVideoExisting, so we supply a version that contains calls to
+	// VideoInput_Protected_Report_No_Signal() and VideoInput_Protected_Report_Signal()
 	IsVideoExistingPolling();
-	// Extract events from the status.
-	VideoInput_Protected_Report_Status(Toshiba_TC358870_HDMI_IsVideoExisting());
 }
 void VideoInput_Reset(uint8_t inputId)
 {
@@ -46,6 +94,7 @@ void VideoInput_Reset(uint8_t inputId)
 	else
 		WriteLn("Wrong HDMI num");
 }
+
 static const char LIBHDK2_NOT_SUPPORTED[] = "TC358870 via libhdk20 does not support this feature.";
 
 void VideoInput_Suspend(void) { WriteLn(LIBHDK2_NOT_SUPPORTED); }

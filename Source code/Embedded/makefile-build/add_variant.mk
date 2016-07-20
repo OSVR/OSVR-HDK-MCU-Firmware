795af345f7d1c62e3a7cb8f47873fd508b11ce60
Ryan Pavlik <ryan@sensics.com>
2016-07-20 17:35:37 -0500

    (split)Build listing file with makefile too.

--- Source code/Embedded/makefile-build/add_variant.mk
+++ Source code/Embedded/makefile-build/add_variant.mk
@@ -21,7 +21,7 @@ FINAL_GENERATED += $(COPIED_OUTPUT_$(VARIANT_NAME))
 #$(info Available variant: $(VARIANT_NAME) aka $(SHORT_NAME), copied output of $(COPIED_OUTPUT_$(VARIANT_NAME)))
 
 # For this target, we pass $< instead of $@ to set_variant_variable because that's the one that still lives in the variant directory.
-$(COPIED_OUTPUT_$(VARIANT_NAME)): $(VARIANT_NAME)/$(OUTPUT_HEX)
+$(COPIED_OUTPUT_$(VARIANT_NAME)): $(VARIANT_NAME)/$(OUTPUT_HEX) $(VARIANT_NAME)/$(OUTPUT_LSS)
 	$(call set_variant_variable,$<)
 	$(QUIETRULE)$(call FUNC_CP,$<,$@)
 	@echo [$(VARIANT)] Copied output hex to $@

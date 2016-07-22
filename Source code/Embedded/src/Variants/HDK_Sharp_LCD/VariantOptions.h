cf819ccb7c1322344917763350bfe64cf12bc05c
VRGuy <yboger@sensics.com>
2016-07-22 12:35:55 -0400

    (split)fix BNO directive in dSight variant

--- Source code/Embedded/src/Variants/HDK_Sharp_LCD/VariantOptions.h
+++ Source code/Embedded/src/Variants/HDK_Sharp_LCD/VariantOptions.h
@@ -18,8 +18,7 @@
 #define SVR_HDK_DEFAULT_MINOR_VER 1
 #define SVR_HDK_VARIANT_STRING "1X-LCD"
 
-// BNO070 IMU (reporting over USB through this MCU)
-#define BNO070
+// no BNO070 IMU (reporting over USB through this MCU), so not included in what the MCU needs to do
 
 // sharp 5.5" LCD
 #define LS055T1SX01

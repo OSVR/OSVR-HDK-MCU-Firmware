e629d40b9314f08f468d6ea8808ed82de1be4157
Ryan Pavlik <ryan@sensics.com>
2016-07-25 17:53:28 -0500

    (split)Better use of ifdefs in SideBySide.

--- Source code/Embedded/src/SideBySide.c
+++ Source code/Embedded/src/SideBySide.c
@@ -12,7 +12,17 @@
 #include "my_hardware.h"
 #include <ioport.h>
 
-static bool sxs_enabled = false;
+#if defined(DSIGHT)
+// dSight defaults to enabled.
+#define SXS_STARTUP_VALUE true
+
+#else
+#define SXS_STARTUP_VALUE false
+#endif
+
+#define SXS_STARTUP_PIN_STATE (SXS_STARTUP_VALUE ? IOPORT_INIT_LOW : IOPORT_INIT_HIGH)
+
+static bool sxs_enabled = SXS_STARTUP_VALUE;
 
 static void SxS_Set(bool state);
 static void SxS_Apply(void);
@@ -22,42 +32,51 @@ static void SxS_Set(bool state)
 {
 	if (state)
 	{
-#ifdef DSIGHT
+#ifdef Side_by_side_A
 		ioport_set_pin_high(Side_by_side_A);
 #endif
+#ifdef Side_by_side_B
 		ioport_set_pin_high(Side_by_side_B);
+#endif
 	}
 	else
 	{
-#ifdef DSIGHT
+#ifdef Side_by_side_A
 		ioport_set_pin_low(Side_by_side_A);
 #endif
+#ifdef Side_by_side_B
 		ioport_set_pin_low(Side_by_side_B);
+#endif
 	}
 }
 
 static inline void SxS_Apply(void) { SxS_Set(sxs_enabled); }
 static inline void SxS_Save_Config()
 {
-#ifdef OSVRHDK
+#ifdef SVR_HAVE_SIDEBYSIDE_CONFIG
 	SetConfigValue(SideBySideOffset, sxs_enabled ? 1 : 0);
 #endif
 }
 
 void SxS_Init()
 {
-#if defined(OSVRHDK)
-	ioport_configure_pin(Side_by_side_B, IOPORT_DIR_OUTPUT | IOPORT_INIT_LOW);
+#ifdef Side_by_side_A
+	// Initialize this pin to FPGA, if defined.
+	ioport_configure_pin(Side_by_side_A, IOPORT_DIR_OUTPUT | SXS_STARTUP_PIN_STATE);
+#endif
+#ifdef Side_by_side_B
+	// Initialize this pin to FPGA, if defined.
+	ioport_configure_pin(Side_by_side_B, IOPORT_DIR_OUTPUT | SXS_STARTUP_PIN_STATE);
+#endif
+
+#ifdef SVR_HAVE_SIDEBYSIDE_CONFIG
+	// If we have config, read and apply it.
 	uint8_t sideBySideConfig;
 	if (GetValidConfigValueOrWriteDefault(SideBySideOffset, 0, &sideBySideConfig))
 	{
 		sxs_enabled = (sideBySideConfig != 0);
 		SxS_Apply();
 	}
-#elif defined(DSIGHT)
-	ioport_configure_pin(Side_by_side_A, IOPORT_DIR_OUTPUT | IOPORT_INIT_HIGH);
-	ioport_configure_pin(Side_by_side_B, IOPORT_DIR_OUTPUT | IOPORT_INIT_HIGH);
-	sxs_enabled = true;
 #endif
 }
 

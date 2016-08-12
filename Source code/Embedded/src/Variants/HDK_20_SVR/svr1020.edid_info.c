221d414848cbf0110da515f73389add175f80c05
Ryan Pavlik <ryan@sensics.com>
2016-08-12 16:26:34 -0500

    Change to just swapping the data section containing the edid binary.
    Weak symbols aren't apparently working.

---
+++ Source code/Embedded/src/Variants/HDK_20_SVR/svr1020.edid_info.c
@@ -0,0 +1,5 @@
+#include "VariantOptions.h"
+
+const char svrEdidInfoString[] = "SVR1020, based on 1.01, all dims 26x15";
+
+/* svr1020.edid.bin swapped in as the contents of section .rodata.EDID_LUT in libhdk20.1.01.a to produce libhdk20.1.01.svr1020.a */

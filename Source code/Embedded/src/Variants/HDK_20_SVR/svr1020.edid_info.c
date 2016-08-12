cc113bc62d0f77f94079e22c6a2507efcb5cafe0
Ryan Pavlik <ryan@sensics.com>
2016-08-12 16:26:34 -0500

    Temporarily switch HDK2SVR to an SVR1019 PNPID/PID to appease NVIDIA direct mode whitelist.

--- Source code/Embedded/src/Variants/HDK_20_SVR/svr1020.edid_info.c
+++ Source code/Embedded/src/Variants/HDK_20_SVR/svr1020.edid_info.c
@@ -1,5 +1,5 @@
 #include "VariantOptions.h"
 
-const char svrEdidInfoString[] = "SVR1020, based on 1.01, all dims 26x15, r2";
+const char svrEdidInfoString[] = "SVR1019, based on 1.01, all dims 26x15 (r3)";
 
 /* svr1020.edid.bin swapped in as the contents of section .rodata.EDID_LUT in libhdk20.1.01.a to produce libhdk20.1.01.svr1020.a */

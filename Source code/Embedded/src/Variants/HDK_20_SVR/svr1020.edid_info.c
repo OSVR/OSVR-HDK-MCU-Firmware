821a9116a743d6c8a6e61ed4c3a2f742fcb7567a
Ryan Pavlik <ryan@sensics.com>
2016-08-19 15:46:29 -0500

    Adjust Atmel Studio project so it can build HDK_20_SVR variant, for debugging.

--- Source code/Embedded/src/Variants/HDK_20_SVR/svr1020.edid_info.c
+++ Source code/Embedded/src/Variants/HDK_20_SVR/svr1020.edid_info.c
@@ -1,5 +1,8 @@
 #include "VariantOptions.h"
 
+// Guard since Atmel Studio doesn't allow conditional compilation of files in just some build configurations
+#ifdef SVR_IS_HDK_20_SVR
 const char svrEdidInfoString[] = "SVR1019, based on 1.01, all dims 26x15 (r3)";
+#endif
 
 /* svr1020.edid.bin swapped in as the contents of section .rodata.EDID_LUT in libhdk20.1.01.a to produce libhdk20.1.01.svr1020.a */

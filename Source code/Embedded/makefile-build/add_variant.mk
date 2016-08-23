1e7f54a8ab844dd4ca01f18436ceffaddd11ddab
Ryan Pavlik <ryan@sensics.com>
2016-08-23 10:48:21 -0500

    Makefile cleanups and improvements.

--- Source code/Embedded/makefile-build/add_variant.mk
+++ Source code/Embedded/makefile-build/add_variant.mk
@@ -48,47 +48,76 @@ ALL_C_SRCS := $(C_SRCS) $(EXTRA_C_SRCS)
 
 C_OBJS := $(addprefix $(BUILD_DIR)/,$(ALL_C_SRCS:%.c=%.o))
 
-# set target-specific variables
-$(BUILD_DIR)/%: SUFFIX := $(SUFFIX)
-$(BUILD_DIR)/%: VARIANT := $(VARIANT)
-
 # Build .c -> .o
 $(BUILD_DIR)/%.o: $(REL_ROOT)/%.c
-	@echo [$(VARIANT)$(SUFFIX)] [-O$(strip $(OPTIMIZATION))] $< : $@
-	$(QUIETRULE)$(call FUNC_MKDIR_P,$(@D))
-	$(QUIETRULE)$(CC) $(call make_include_dirs,$(VARIANT)) $(ALL_CFLAGS) -O$(strip $(OPTIMIZATION)) -MD -MP -MF "$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)"   -o "$@" "$<"
+	$(CANNED_RECIPE_BEGINNING_SHOW_IN_AND_OUT)
+	$(QUIETRULE)$(CC) $(call make_include_dirs,$(VARIANT)) $(ALL_CFLAGS) -O$(strip $(OPTIMIZATION)) -MD -MP -MF "$(@:o=d)" -MT"$(@:o=d)" -MT"$(@:o=o)" -MT"$(@:o=i)"  -o "$@" "$<"
 
-S_OBJS := $(addprefix $(BUILD_DIR)/,$(PREPROCESSING_SRCS:%.s=%.o))
+S_OBJS := $(addprefix $(BUILD_DIR)/,$(PREPROCESSING_SRCS:s=o))
 
 # Pattern for building object files from .s
 $(BUILD_DIR)/%.o: $(REL_ROOT)/%.s
-	@echo [$(VARIANT)$(SUFFIX)] $< : $@
-	$(QUIETRULE)$(call FUNC_MKDIR_P,$(@D))
-	$(QUIETRULE)$(CC) $(call make_include_dirs,$(VARIANT)) $(ALL_ASFLAGS) -MD -MP -MF "$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -Wa,-g -o "$@" "$<"
+	$(CANNED_RECIPE_BEGINNING_SHOW_IN_AND_OUT)
+	$(QUIETRULE)$(CC) $(call make_include_dirs,$(VARIANT)) $(ALL_ASFLAGS) -MD -MP -MF "$(@:o=d)" -MT"$(@:o=d)" -MT"$(@:o=o)" -Wa,-g -o "$@" "$<"
 
 CURRENT_OBJS := $(C_OBJS) $(S_OBJS)
 
+# Pattern for outputting preprocessed files from .c files
+# Additional flags are to disable warnings, turn on preprocessor-only output, -dDI to show define directives and results of preprocessing as well as include directives, -C to include comments
+$(BUILD_DIR)/%.i: $(REL_ROOT)/%.c
+	$(CANNED_RECIPE_BEGINNING_SHOW_IN_AND_OUT)
+	$(QUIETRULE)$(CC) $(call make_include_dirs,$(VARIANT)) $(ALL_CFLAGS) -w -E -dDI -C -o "$@" "$<"
+
+# For all non-ASF files, make a shortcut phony target that's just $(BUILD_DIR)/basename.i to keep the typing shorter.
+define make_preprocess_phony_target
+# only if this would actually be a shortcut and not just replace the real target
+ifneq ($(strip $(dir $1)),./)
+.PHONY: $(BUILD_DIR)/$(basename $(notdir $1)).i
+$(BUILD_DIR)/$(basename $(notdir $1)).i: $(BUILD_DIR)/$(1:c=i)
+endif
+endef
+# We skip ASF files here because there are lots of duplicate filenames, and generally we suspect they aren't as interesting to preprocess anyway.
+$(foreach src,$(filter-out src/ASF/%,$(ALL_C_SRCS)),$(eval $(call make_preprocess_phony_target,$(src))))
+
 # Makes the elf file
 $(BUILD_DIR)/$(OUTPUT_FILE_PATH): $(CURRENT_OBJS) $(LIBS) $(LIBS_$(VARIANT))
-	@echo [$(VARIANT)$(SUFFIX)] Linking $@
+	$(CANNED_RECIPE_BEGINNING_SHOW_OUT)
 	$(QUIETRULE)$(CC) -o"$@" $^ $(LIBS) $(LIBS_$(VARIANT)) -Wl,-Map="$(BUILD_DIR)/$(OUTPUT_MAP)" -Wl,--start-group -Wl,-lm  -Wl,--end-group -Wl,--gc-sections -mrelax -mmcu=$(MCU) -Wl,--relax -Wl,--section-start=.BOOT=0x40000
 	$(QUIETRULE)$(AVRSIZE) --mcu=$(MCU) --format=avr "$@"
+%.elf: DISPLAY_OP := Linking
 
+###
 # Compiler database generation - optional, for clang tool usage.
+# requires the "jq" tool v1.5 or newer to make the "compile_commands"
+
+COMPILATION_DATABASE_FILENAME := compile_commands.json
+COMPILATION_DATABASE_PARAMS := command_parts.json
 json_stringify = jq -R "." >>"$@"
 
-$(BUILD_DIR)/command_parts.json: Makefile add_variant.mk
-	@echo [$(VARIANT)$(SUFFIX)] $@
-	$(QUIETRULE)$(call FUNC_MKDIR_P,$(@D))
+# the PARAMS get sucked in as a string array: lines as follows:
+# the main part of the compile command (some must be manually kept in sync!)
+# "build directory" (makefile dir) absolute path
+# output build directory - the thing we append to the source paths to get the object paths
+# the absolute source root
+EXTRA_CLEAN_FILES += $(BUILD_DIR)/$(COMPILATION_DATABASE_PARAMS)
+$(BUILD_DIR)/$(COMPILATION_DATABASE_PARAMS): Makefile add_variant.mk
+	$(CANNED_RECIPE_BEGINNING_SHOW_OUT)
 	$(QUIETRULE)-$(RM) "$@"
 	$(QUIETRULE)echo $(CC) $(call make_include_dirs,$(VARIANT)) $(ALL_CFLAGS) -O$(strip $(OPTIMIZATION)) -MD -MP | $(json_stringify)
 	$(QUIETRULE)echo $(abspath $(strip .))| $(json_stringify)
 	$(QUIETRULE)echo $(abspath $(strip $(BUILD_DIR)))| $(json_stringify)
 	$(QUIETRULE)echo $(abspath $(REL_ROOT))| $(json_stringify)
 
-$(BUILD_DIR)/compile_commands.json: make-compiledatabase.jq $(BUILD_DIR)/command_parts.json add_variant.mk
-	@echo [$(VARIANT)$(SUFFIX)] $@
-	$(QUIETRULE)echo $(subst $(eval) ,;,$(strip $(ALL_C_SRCS))) | jq --raw-input --slurp --slurpfile params "$(BUILD_DIR)/command_parts.json" -f "$<" > "$@"
+# This rule takes the makefile variable with all the sources, turns it into a long semicolon-delimited string,
+# and feeds it into the "jq" tool, along with the PARAMS (via the command line), and a jq script (also via command line arg)
+# which does the dirty work of turning it into something that looks like it might have come out of CMake.
+EXTRA_CLEAN_FILES += $(BUILD_DIR)/$(COMPILATION_DATABASE_FILENAME)
+$(BUILD_DIR)/$(COMPILATION_DATABASE_FILENAME): make-compiledatabase.jq $(BUILD_DIR)/$(COMPILATION_DATABASE_PARAMS) add_variant.mk
+	$(CANNED_RECIPE_BEGINNING_SHOW_OUT)
+	$(QUIETRULE)echo $(subst $(eval) ,;,$(strip $(ALL_C_SRCS))) | jq --raw-input --slurp --slurpfile params "$(BUILD_DIR)/$(COMPILATION_DATABASE_PARAMS)" -f "$<" > "$@"
+#
+# end clang/libtooling section
+###
 
 # Add targets for different configurations of the build.
 CONFIG_SHORT_NAME := hdmi_verbose
@@ -105,6 +134,11 @@ CONFIG_SHORT_NAME := bno_dfu
 DEFINES_CONTENTS := PERFORM_BNO_DFU
 SKIP_VARIANTS := dSight_Sharp_LCD HDK_Sharp_LCD
 include add_config.mk
+
+
+# set target-specific variables - have to do this here, where these are not recursively defined.
+$(BUILD_DIR)/%: SUFFIX := $(SUFFIX)
+$(BUILD_DIR)/%: VARIANT := $(VARIANT)
 
 # Clean up work variables
 C_OBJS :=

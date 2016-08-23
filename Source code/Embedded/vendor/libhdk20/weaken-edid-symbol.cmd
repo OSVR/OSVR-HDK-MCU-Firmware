a7e14d309621ff3b89361564e7c8f4e428be8eec
Ryan Pavlik <ryan@sensics.com>
2016-08-23 10:48:21 -0500

    Remove old libhdk20 binaries now that they are unneeded.

--- Source code/Embedded/vendor/libhdk20/weaken-edid-symbol.cmd
+++
@@ -1,2 +0,0 @@
-rem Copies the library to a new name, in the process, weakens the EDID_LUT symbol, renames the existing data section for it
-avr-objcopy --weaken-symbol .rodata.EDID_LUT --weaken-symbol EDID_LUT --rename-section .rodata.EDID_LUT=.rodata.EDID_LUT_OLD %1 %~n1.weakedid.a

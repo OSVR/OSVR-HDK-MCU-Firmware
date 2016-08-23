94931bdc401ad6cc7e95ce111159ad4c3cb960e3
Ryan Pavlik <ryan@sensics.com>
2016-08-23 10:48:21 -0500

    Add token pasting macros to macro utils.

--- Source code/Embedded/src/MacroUtils.h
+++ Source code/Embedded/src/MacroUtils.h
@@ -14,4 +14,31 @@
 /** @brief Utility macro for stringification of macro expansions. */
 #define SVR_STRINGIFY(X) SVR_STRINGIFY_IMPL(X)
 
+/** @brief Utility macro used in concatenation. */
+#define SVR_CAT_IMPL(X) X
+
+/** @brief Utility macro for token pasting aka concatenation */
+#define SVR_CAT(A, B) SVR_CAT_IMPL(A##B)
+
+/** @brief Utility macro for token pasting aka concatenation following macro expansion */
+#define SVR_EXPAND_CAT(A, B) SVR_CAT(A, B)
+
+/** @brief Utility macro for token pasting aka concatenation */
+#define SVR_CAT3(A, B, C) SVR_CAT_IMPL(A##B##C)
+
+/** @brief Utility macro for token pasting aka concatenation following macro expansion */
+#define SVR_EXPAND_CAT3(A, B, C) SVR_CAT3(A, B, C)
+
+/** @brief Utility macro for token pasting aka concatenation */
+#define SVR_CAT4(A, B, C, D) SVR_CAT_IMPL(A##B##C##D)
+
+/** @brief Utility macro for token pasting aka concatenation following macro expansion */
+#define SVR_EXPAND_CAT4(A, B, C, D) SVR_CAT4(A, B, C, D)
+
+/** @brief Utility macro for token pasting aka concatenation */
+#define SVR_CAT5(A, B, C, D, E) SVR_CAT_IMPL(A##B##C##D##E)
+
+/** @brief Utility macro for token pasting aka concatenation following macro expansion */
+#define SVR_EXPAND_CAT5(A, B, C, D, E) SVR_CAT5(A, B, C, D, E)
+
 #endif /* MACROUTILS_H_ */

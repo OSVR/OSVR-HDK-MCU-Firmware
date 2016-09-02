46b79d2977a255744ee82ea1a1cd33cefde2f5e4
Ryan Pavlik <ryan@sensics.com>
2016-09-02 14:40:05 -0500

    Add more debugging

--- Source code/Embedded/src/DeviceDrivers/VideoInput_Toshiba_TC358870.c
+++ Source code/Embedded/src/DeviceDrivers/VideoInput_Toshiba_TC358870.c
@@ -125,17 +125,70 @@ static const char LIBHDK2_NOT_SUPPORTED[] = "TC358870 via libhdk20 does not supp
 void VideoInput_Suspend(void) { WriteLn(LIBHDK2_NOT_SUPPORTED); }
 void VideoInput_Resume(void) { WriteLn(LIBHDK2_NOT_SUPPORTED); }
 void VideoInput_Poll_Status(void) { VideoInput_Protected_Report_Status(Toshiba_TC358870_Have_Video_Sync()); }
+#define VITC_GETBIT(VAL, BIT) ((uint8_t)(((VAL & BITUTILS_BIT(BIT)) >> (BIT))))
+
+static inline void vi_tc_dump_reg_status(uint16_t reg, uint8_t bytes)
+{
+	char myMessage[50];
+	sprintf(myMessage, "TC358870 reg %#06" PRIX16 ": ", reg);
+	Write(myMessage);
+
+	uint32_t data = 0;
+	Toshiba_TC358870_I2C_Read32(reg, &data);
+
+	// access through a character pointer is OK even with strict aliasing.
+	unsigned char* buf = (unsigned char*)(&data);
+
+	switch (bytes)
+	{
+	case 1:
+	{
+		sprintf(myMessage, " (as 8-bit hex %#04" PRIx8 ") ", buf[0]);
+		Write(myMessage);
+		break;
+	}
+	case 2:
+	{
+		// not violating strict aliasing...
+		uint16_t justMyData;
+		unsigned char* myBuf = (unsigned char*)(&justMyData);
+		myBuf[0] = buf[0];
+		myBuf[1] = buf[1];
+
+		sprintf(myMessage, " (as 16-bit hex %#06" PRIx16 ") ", justMyData);
+		Write(myMessage);
+	}
+
+	case 4:
+	{
+		sprintf(myMessage, " (as 32-bit hex %#010" PRIx32 ") ", data);
+		Write(myMessage);
+		break;
+	}
+	}
+
+	for (uint8_t i = 0; i < bytes; ++i)
+	{
+		unsigned char val = buf[bytes - i - 1];
+		sprintf(myMessage, "%d%d%d%d %d%d%d%d ", VITC_GETBIT(val, 7), VITC_GETBIT(val, 6), VITC_GETBIT(val, 5),
+		        VITC_GETBIT(val, 4), VITC_GETBIT(val, 3), VITC_GETBIT(val, 2), VITC_GETBIT(val, 1),
+		        VITC_GETBIT(val, 0));
+		Write(myMessage);
+	}
+	WriteEndl();
+}
+
 // to console
 void VideoInput_Report_Status(void)
 {
 	Write("Video input status: ");
 	if (VideoInput_Get_Status())
 	{
-		WriteLn(" signal available.");
+		WriteLn("have sync.");
 	}
 	else
 	{
-		WriteLn("no signal available.");
+		WriteLn("no sync available.");
 	}
 	char myMessage[50];
 	sprintf(myMessage, "TC358870_Init called %d times", Toshiba_TC358870_Get_Init_Count());
@@ -148,16 +201,24 @@ void VideoInput_Report_Status(void)
 		Toshiba_TC358870_I2C_Read16(0x0000, &data);
 		uint8_t highByte = (uint8_t)((data >> 8) & 0xff);
 		uint8_t lowByte = (uint8_t)(data & 0xff);
-		sprintf(myMessage, "Chip ID: %#04" PRIX8 " (expected 0x47)", highByte);
+		sprintf(myMessage, "Chip ID: %#04" PRIx8 " (expected 0x47)", highByte);
 		WriteLn(myMessage);
-		sprintf(myMessage, "Rev ID: %#04" PRIX8 " (expected 0x00)", lowByte);
-		WriteLn(myMessage);
-	}
-	{
-		uint8_t data = 0;
-		Toshiba_TC358870_I2C_Read8(0x8520, &data);
-		sprintf(myMessage, "System status reg 0x8520: %#04" PRIX8, data);
+		sprintf(myMessage, "Rev ID: %#04" PRIx8 " (expected 0x00)", lowByte);
 		WriteLn(myMessage);
 	}
+	Write("System Status ");
+	vi_tc_dump_reg_status(0x8520, 1);
+
+	Write("LANE_STATUS_HS (DSI-TX0) ");
+	vi_tc_dump_reg_status(0x0290, 4);
+
+	Write("LANE_STATUS_LS (DSI-TX0) ");
+	vi_tc_dump_reg_status(0x0294, 4);
+
+	Write("MIPI_PLL_CNF (DSI-TX0) ");
+	vi_tc_dump_reg_status(0x02AC, 4);
+
+	Write("FUNC_MODE (DSI-TX0) ");
+	vi_tc_dump_reg_status(0x0150, 4);
 }
 #endif

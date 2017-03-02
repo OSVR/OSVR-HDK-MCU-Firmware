30d5598c3fa44ac4eba3561f7164879148ff7722
Ryan Pavlik <ryan@sensics.com>
2017-03-02 10:06:25 -0600

    Remove now-redundant register declarations for solomon

--- Source code/Embedded/src/DeviceDrivers/Solomon.h
+++ Source code/Embedded/src/DeviceDrivers/Solomon.h
@@ -10,6 +10,8 @@
 
 #include "GlobalOptions.h"
 #include "DeviceDrivers/Display.h"
+#include "DeviceDrivers/Solomon_SSD2828.h"
+
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -34,30 +36,6 @@ void Solomon_Reset(uint8_t SolomonNum);
 void Solomon_Dump_All_Config_Debug(const char* loc);
 /// Writes out the config register to the console for just one device.
 void Solomon_Dump_Config_Debug(uint8_t deviceId, const char* loc);
-
-#define SOLOMON_REG_CFGR UINT8_C(0xB7)
-static const uint16_t SOLOMON_CFGR_TXD_bm = (UINT16_C(0x01) << 11);
-static const uint16_t SOLOMON_CFGR_LPE_bm = (UINT16_C(0x01) << 10);
-static const uint16_t SOLOMON_CFGR_REN_bm = (UINT16_C(0x01) << 7);
-static const uint16_t SOLOMON_CFGR_DCS_bm = (UINT16_C(0x01) << 6);
-
-/// Clock select - only touch when PEN is off.
-static const uint16_t SOLOMON_CFGR_CSS_bm = (UINT16_C(0x01) << 5);
-/// video enable
-static const uint16_t SOLOMON_CFGR_VEN_bm = (UINT16_C(0x01) << 3);
-/// Sleep/ULP mode
-static const uint16_t SOLOMON_CFGR_SLP_bm = (UINT16_C(0x01) << 2);
-/// clock lane enable high speed in all cases
-static const uint16_t SOLOMON_CFGR_CKE_bm = (UINT16_C(0x01) << 1);
-/// Send data using HS interface
-static const uint16_t SOLOMON_CFGR_HS_bm = (UINT16_C(0x01));
-
-/// Packet drop register
-#define SOLOMON_REG_PDR UINT8_C(0xBF)
-/// PLL control reg
-#define SOLOMON_REG_PCR UINT8_C(0xBF)
-/// PLL enable
-static const uint16_t SOLOMON_PCR_PEN_bm = (UINT16_C(0x01));
 
 // read the solomon ID
 uint16_t read_Solomon_ID(uint8_t channel);
